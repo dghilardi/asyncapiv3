fe685de993adea9e23fc8caca43e53fa0d64270f
xd009642 <danielmckenna93@gmail.com>
2025-06-30 11:00:29 +0900

    Apply auto clippy fix

--- tests/spec_v3_deserialization.rs
+++ tests/spec_v3_deserialization.rs
@@ -8,14 +8,13 @@ use std::path::Path;
 fn deserialize_asyncapi_v3_examples() {
     let mut paths = fs::read_dir("./test-res/3.0.0")
         .unwrap()
-        .into_iter()
         .map(|entry| entry.expect("Cannot read direntry").path())
         .collect::<Vec<_>>();
 
     paths.sort();
 
     for path in paths {
-        deserialize_spec(&path).expect(&format!("Cannot deserialize spec {path:?}"));
+        deserialize_spec(&path).unwrap_or_else(|_| panic!("Cannot deserialize spec {path:?}"));
 
         println!(" * {path:?} - Deserialized");
     }
@@ -24,9 +23,8 @@ fn deserialize_asyncapi_v3_examples() {
 fn deserialize_spec(entry: &Path) -> Result<AsyncApiSpec, Box<dyn std::error::Error>> {
     let file = File::open(entry)?;
     let reader = BufReader::new(file);
-    let spec = serde_yaml::from_reader::<_, AsyncApiSpec>(reader).map_err(|err| {
+    let spec = serde_yaml::from_reader::<_, AsyncApiSpec>(reader).inspect_err(|err| {
         println!("Error location: {:?}", err.location());
-        err
     })?;
     Ok(spec)
 }
